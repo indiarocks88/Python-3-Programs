68772122a86d8534ec649412c32242d1dbcd6e85
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-02-13 18:03:52 +0530

    Python 3 Hello World Program

---
+++ HelloPi1.py
@@ -0,0 +1,21 @@
+"""
+Program: Python 3 program to display the "Hello World!" message on the console screen.
+Date: Friday, 12-02-2021
+@author: Ankur Saxena
+Platform: Windows 10 Pro/x64/Python 3.8/Atom
+"""
+
+# Program start
+
+# Print message
+print ("Hello World!")
+
+# Program end
+
+"""
+Save this file as "HelloPi1.py"
+Execute: $ python HelloPi1.py [hit Enter]
+Output:
+
+Hello World!
+"""

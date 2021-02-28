b178ffc0145a30e5ddf12ce5401dc23860ffd5ba
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-03-01 02:43:20 +0530

    Python 3 Input String Program
    Write a Python 3 program that accepts two lines from the user and display on the console screen.

---
+++ InputStr1.py
@@ -0,0 +1,29 @@
+"""
+Program: Write a Python 3 program that accepts two lines from the user and display on the console screen.
+Date: Monday, 01-03-2021
+@author: Ankur Saxena
+Platform: Windows 10 Pro/x64/Python 3.6/Vim editor
+"""
+
+# program start
+
+# creating a variable
+takeInput1 = input()
+takeInput2 = input()
+
+# printing variable content
+print (takeInput1)
+print (takeInput2)
+
+# program end
+
+# Save this file as "InputStr1.py"
+# Execute: $ python InputStr1.py [hit Enter]
+
+"""
+Output:
+
+Hello Python 3!
+I am coming back to the race.
+"""
+

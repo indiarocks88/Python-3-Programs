bd33e0c78d6ac6e6ae1381735bda59b60f8aebfc
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-03-03 14:50:37 +0530

    Python 3 Multiline Program Using "\n"
    Write a Python 3 program to print the following multiline string on the console screen using single "print ()" command:
    
        Hello World!
        Welcome to the world of Python 3 programming.
        Python 3 programming is fun...
        Let us learn Python 3 programming!!

---
+++ MultilineStr1.py
@@ -0,0 +1,42 @@
+"""
+Program: Write a Python 3 program to print the following multiline string on the console screen using single "print ()" command:
+
+    Hello World!
+    Welcome to the world of Python 3 programming.
+    Python 3 programming is fun...
+    Let us learn Python 3 programming!!
+"""
+
+"""
+Date: Wednesday, 3-3-2021
+@author: Ankur Saxena
+Platform: Windows 10 Pro/x64/Python3.6/Vim editor
+"""
+
+# program start
+# using "\n" escape character
+
+# import python module
+import os
+
+os.system ("cls") # used to clear the console screen
+
+print ("Hello World!\nWelcome to the world of Python 3 programming.\nPython 3 programming is fun...\nLet us learn Python 3 programming!!\n")
+
+# program end
+
+# Save this file as "MultilineStr1.py"
+# Execute: $ python MultilineStr1.py [hit Enter]
+
+"""
+Output:
+
+Hello World!
+Welcome to the world of Python 3 programming.
+Python 3 programming is fun...
+Let us learn Python 3 programming!!
+
+
+Press ENTER or type command to continue
+"""
+

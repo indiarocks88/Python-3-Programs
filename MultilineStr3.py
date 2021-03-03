bbe9ce54c68e8dfb154d5ad092580d1b6ce68d1c
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-03-03 14:57:20 +0530

    Python 3 Multiline String Using three single-quotes
    Write a Python 3 program to print the following multiline string on the console screen using single "print ()" command:
    
        Hello World!
        Welcome to the world of Python 3 programming.
        Python 3 programming is fun...
        Let us learn Python 3 programming!!

---
+++ MultilineStr3.py
@@ -0,0 +1,47 @@
+"""
+Program: Write a Python 3 program to print the following multiline string on the console screen using single "print ()" command:
+
+    Hello World!
+    Welcome to the world of Python 3 programming.
+    Python 3 programming is fun...
+    Let us learn Python 3 programming!!
+"""
+
+"""
+Date: Wednesday, 3-3-2021
+@author: Ankur Saxena
+Platform: Windows 10 Pro/x64/Python3.6/Vim editor
+"""
+
+# program start
+# using three single-quotes
+
+# import python module
+import os
+
+os.system ("cls") # used to clear the console screen
+
+print ('''
+Hello World!
+Welcome to the world of Python 3 programming.
+Python 3 programming is fun...
+Let us learn Python 3 programming!!
+''')
+
+# program end
+
+# Save this file as "MultilineStr3.py"
+# Execute: $ python MultilineStr3.py [hit Enter]
+
+"""
+Output:
+
+Hello World!
+Welcome to the world of Python 3 programming.
+Python 3 programming is fun...
+Let us learn Python 3 programming!!
+
+
+Press ENTER or type command to continue
+"""
+

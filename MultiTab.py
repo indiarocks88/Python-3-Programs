85f91ca145475557d77f32c79487e30330f2e484
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-03-23 13:49:49 +0530

    Python 3 Multiplication Of Two Numbers Program

---
+++ MultiTab.py
@@ -0,0 +1,39 @@
+"""
+Program: Python 3 program to calculate and display the multiplication of two numbers on the console screen.
+Date: Tue, 23-03-2021
+@author: Ankur Saxena
+Platform: Windows 10 Pro/x64/Python 3.6/Vim editor
+"""
+
+# program start
+
+num1 = input ("Please enter first number:\n")
+num2 = input ("Please enter second number:\n")
+
+# calculate multiplication
+result = int(num1) * int(num2)
+
+# print result
+print ("\nThe multiplication of ", num1, " and ", num2, " is: ", result)
+
+print ("\n\nThanks for using my application!\n")
+
+# program end
+
+# Save this file as "MultiTab.py"
+# Execute: $ python MultiTab.py [hit Enter]
+
+"""
+Output:
+
+Please enter first number:
+123
+Please enter second number:
+15
+
+The multiplication of  123  and  15  is:  1845
+
+
+Thanks for using my application!
+"""
+

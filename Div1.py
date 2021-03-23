37415c1bd6773317f968ada5f94acda3ab73fb69
Ankur Saxena <46398844+indiarocks88@users.noreply.github.com>
2021-03-23 14:15:05 +0530

    Python 3 Program: Division Of Two Numbers

---
+++ Div1.py
@@ -0,0 +1,39 @@
+"""
+Program: Python 3 program to calculate the division of two numbers entered by the user.
+Date: Tue, 23-03-2021
+@author: Ankur Saxena
+Platform: Windows 10 Pro/x64/Python 3.6/Vim editor
+"""
+
+# program start
+
+num1 = input ("Please enter first number:\n")
+num2 = input ("Please enter second number:\n")
+
+# calculate division
+result = float(num1) * float(num2)
+
+# print result
+print ("\nThe division of ", num1, " and ", num2, " is: ", result)
+
+print ("\n\nThanks for using my application!\n")
+
+# program end
+
+# Save this file as "Div1.py"
+# Execute: $ python Div1.py [hit Enter]
+
+"""
+Output:
+
+Please enter first number:
+125
+Please enter second number:
+5
+
+The division of  125  and  5  is:  625.0
+
+
+Thanks for using my application!
+"""
+
